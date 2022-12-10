b4b91a93581ba745b1ac63b751e9179de511e13a
Michael Synan <mforest@protonmail.com>
2022-12-10 12:56:49 -0500

    media query updates

--- src/components/layouts/TheHero.vue
+++ src/components/layouts/TheHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col gap-8 h-screen">
     <h1
-      class="xs:pb-40 sm:pb-40 pb-20 m-auto text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text"
+      class="xs:pb-60 sm:pb-60 md:pb-30 lg:pb-30 pb-30 m-auto text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text"
     >
       Full Stack Developer &<br> Project Manager<span
         class="text-link_text_hover"
